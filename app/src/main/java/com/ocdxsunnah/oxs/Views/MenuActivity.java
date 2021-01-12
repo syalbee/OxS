c2ec9a899fac8ca24480d8e32ce064a6fe318173
ocdxsunnah <ocdxsunnah@gmail.com>
2021-01-12 15:05:26 +0700

    hadi latihan update

---
+++ app/src/main/java/com/ocdxsunnah/oxs/Views/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.ocdxsunnah.oxs.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ocdxsunnah.oxs.R;
+
+public class MenuActivity extends AppCompatActivity {
+    private String Menu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
